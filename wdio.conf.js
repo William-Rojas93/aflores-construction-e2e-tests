fe425b4abd0a69a90eb7726fd6cc4a19398c3932
William-Rojas93 <william.ur@aol.com>
2025-09-16 12:56:37 -0600

    updated config file to have proper path for test, ran test and executed test properly

--- wdio.conf.js
+++ wdio.conf.js
@@ -22,6 +22,7 @@ exports.config = {
     //
     specs: [
         // ToDo: define location for spec files here
+        './tests/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -100,7 +101,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: ['devtools'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
